006570521ab67104d6b402dade4cf09fd80cb49e
Eunice <eunj1012@gmail.com>
2022-09-19 23:04:59 -0400

    second commit

--- assignment2.js
+++ assignment2.js
@@ -1,12 +1,29 @@
+var inputElement = document.getElementById("user-input");
+
 
 function displayMessage(){
-    var input = document.getElementById("user-input").value;
-    input += " aloha";
-
-    document.getElementById("user-output").innerHTML = input;
-
-    console.log("it's working!");
-    console.log(input);
+    var input = inputElement.value;
+    var sentence = input.split(" ");
+    var pigTranslation = [];
+    
+    for (let i=0; i <= sentence.length - 1; i++){
+        console.log(sentence[i]);
+        pigTranslation.push(pigWord(sentence[i]));
+    };
+    output = pigTranslation.join(" ");
+    document.getElementById("user-output").innerHTML = output;
 }
 
-document.getElementById("user-input").oninput= displayMessage;
+function pigWord(word){
+    var vowelRE = word.match(/[aeiou]/i);
+    var vowelPos = word.indexOf(vowelRE);
+
+    if (vowelPos > 0) {
+        output = word.slice(vowelPos) + word.slice(0,vowelPos) + "ay";
+    } else {
+        output = word + "way";
+    } 
+    return output;
+}
+
+inputElement.oninput = displayMessage;
